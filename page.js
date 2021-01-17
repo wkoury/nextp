af0ed36daf431ecde61207ef09451d2f5b993b82
Will Koury <wkoury@hey.com>
2021-01-17 14:22:38 -0500

    Initial commit

---
+++ page.js
@@ -0,0 +1,25 @@
+
+exports.generateContent = function (name){
+	return (
+`import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function ${name}() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}`
+);
+
+}
